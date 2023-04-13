8be26a6a7d3616ca18f5708f8af2c73e782b5ce9
Solkind <119994828+Solkind@users.noreply.github.com>
2023-04-13 00:05:20 -0400

    infinite scroll functionality, ReactModal setup and listeners

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
 import BlogItem from "../blog/blog-item";
+import BlogModal from "../modals/blog-modal";
 
 class Blog extends Component {
     constructor() {
@@ -12,22 +13,42 @@ class Blog extends Component {
             blogItems: [],
             totalCount: 0,
             currentPage: 0,
-            isLoading: true
+            isLoading: true,
+            blogModalIsOpen: false
         };
 
         this.getBlogItems = this.getBlogItems.bind(this);
-        this.activateInfiniteScroll();
+        this.onScroll = this.onScroll.bind(this);
+        window.addEventListener("scroll", this.onScroll, false);
+        this.handleNewBlogClick = this.handleNewBlogClick.bind(this);
+        this.handleModalClose = this.handleModalClose.bind(this);
     }
 
-    activateInfiniteScroll() {
-        window.onscroll = () => {
-            if (
-                window.innerHeight + document.documentElement.scrollTop === 
-                document.documentElement.offsetHeight
-            ) {
-                console.log("get more posts");
-            }
-        };
+    handleModalClose() {
+        this.setState({
+            blogModalIsOpen: false
+        });
+    }
+
+    handleNewBlogClick() {
+        this.setState({
+            blogModalIsOpen: true
+        });
+    }
+
+    onScroll() {
+        if (
+            this.state.isLoading ||
+            this.state.blogItems.length === this.state.totalCount
+        ) {
+            return;
+        }
+        if (
+            window.innerHeight + document.documentElement.scrollTop === 
+            document.documentElement.offsetHeight
+        ) {
+            this.getBlogItems();
+        }
     }
 
     getBlogItems() {
@@ -35,22 +56,33 @@ class Blog extends Component {
             currentPage: this.state.currentPage + 1
         });
 
-        axios.get("https://jaredmitchell.devcamp.space/portfolio/portfolio_blogs", {           withCredentials: true
-        })
-        .then((response) => {
-            this.setState({
-                blogItems: response.data.portfolio_blogs,
-                totalCount: response.data.meta.total_records,
-                isLoading: false
+        axios
+            .get(
+                `https://jaredmitchell.devcamp.space/portfolio/portfolio_blogs?page=${this
+                    .state.currentPage}`,
+                {   
+                    withCredentials: true
+                }
+            )
+            .then((response) => {
+                console.log("getBlogItems response", response);
+                this.setState({
+                    blogItems: this.state.blogItems.concat(response.data.portfolio_blogs),
+                    totalCount: response.data.meta.total_records,
+                    isLoading: false
+                });
             })
-        })
-        .catch((error) => {
-            console.log("getBlogItems error", error);
-        });
+            .catch((error) => {
+                console.log("getBlogItems error", error);
+            });
     }
 
     componentWillMount() {
         this.getBlogItems();
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("scroll", this.onScroll, false);
     }
 
     render() {
@@ -60,8 +92,18 @@ class Blog extends Component {
 
         return (
             <div className="blog-container">
-                <div className="content-container">{blogRecords}</div>
+                <BlogModal
+                    handleModalClose={this.handleModalClose}
+                    modalIsOpen={this.state.blogModalIsOpen} 
+                />
 
+                <div className="new-blog-link">
+                    <a onClick={this.handleNewBlogClick}>
+                        Open Modal!
+                    </a>
+                </div>
+
+                <div className="content-container">{blogRecords}</div>
                 {this.state.isLoading ? (
                     <div className="content-loader">
                         <FontAwesomeIcon icon="spinner" spin />
