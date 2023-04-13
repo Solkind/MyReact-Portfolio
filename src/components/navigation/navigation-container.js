d5820b8cd18d324eb2626e5a95d3fedf2225703b
Solkind <119994828+Solkind@users.noreply.github.com>
2023-04-12 21:32:30 -0400

    blog styles, loading, and infinite scrolling structure

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // Higher order component on line 4
 import { withRouter } from "react-router";
 import { NavLink } from "react-router-dom";
@@ -50,8 +51,7 @@ const NavigationComponent = (props) => {
                 
             <div className="right-side">
                 JARED MITCHELL
-
-                {props.loggedInStatus === "LOGGED_IN" ? <a onClick={handleSignOut}>Sign Out</a> : null}
+                {props.loggedInStatus === "LOGGED_IN" ? <a onClick={handleSignOut}><FontAwesomeIcon icon="sign-out-alt" /></a> : null}
             </div>
         </div>
     )
